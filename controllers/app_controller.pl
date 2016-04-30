1e6439549ada280b793c009637917f2725ceb926
marcelobns <marcelobns@live.com>
2016-04-30 19:51:20 -0400

    ajustes de nomenclatura

---
+++ controllers/app_controller.pl
@@ -0,0 +1,32 @@
+:- module('controllers/app_controller', [default_index/0, default_answer/3]).
+:- use_module(library(st/st_render)).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/html_write)).
+:- use_module('../models/app_model').
+
+default_index() :-
+    json_to_dict('prolog_quiz/models/perguntas.json', Perguntas),
+    json_to_dict('prolog_quiz/models/estados.json', Estados),
+
+    format('Content-Type: text/html; charset=UTF-8~n~n'),
+    current_output(Out),
+    st_render_file('prolog_quiz/web/index', _{
+            title: 'Quiz de Geografia',
+            perguntas: Perguntas,
+            estados: Estados
+        }, Out, _{ frontend: semblance }
+    )
+.
+
+default_answer(Id, Key, Value) :-
+    json_to_dict('prolog_quiz/models/estados.json', Estados),
+    member(Dict, Estados),
+
+    atom_string(Id, Id_s), atom_string(Value, Value_s),
+    select_dict(_{}.put([id=Id_s, Key=Value_s]), Dict, _),
+
+    format('Content-Type: text/html; charset=UTF-8~n~n'),
+    format('true')
+.
---
+++ models/app_model.pl
@@ -0,0 +1,9 @@
+:- module('models/app_model', [json_to_dict/2]).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(http/http_json)).
+
+json_to_dict(FilePath, Dict) :-
+    open(FilePath,read,Str),
+    json_read_dict(Str,Dict),
+    close(Str).
