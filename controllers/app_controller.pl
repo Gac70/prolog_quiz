cb383af6704e82697671ab078b72917c0caeb02f
marcelobns <marcelobns@live.com>
2016-05-10 22:16:20 -0400

    linha necessária

--- controllers/app_controller.pl
+++ controllers/app_controller.pl
@@ -21,10 +21,11 @@ index() :-
 .
 
 answer(Id, Key, Value) :-
-    json_to_dict('prolog_quiz/models/estados.json', EstadosDict),
+    json_to_dict('prolog_quiz/models/estados.json', Estados),
+    member(Dict, Estados),
 
     atom_string(Id, Id_s), atom_string(Value, Value_s),
-    select_dict(_{}.put([id=Id_s, Key=Value_s]), EstadosDict, _),
+    select_dict(_{}.put([id=Id_s, Key=Value_s]), Dict, _),
 
     format('Content-Type: text/html; charset=UTF-8~n~n'),
     format('true')
